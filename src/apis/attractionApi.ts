8df9b2ce2c157e1b397de1aca600caaa73cd1fe2
bomeiweiwei <bomeilonch0210@gmail.com>
2026-06-22 16:23:19 +0800

    modify api

--- src/apis/attractionApi.ts
+++ src/apis/attractionApi.ts
@@ -4,20 +4,19 @@ import attractionsMock from "../mocks/attractions.json";
 
 import type { Attraction } from "../types/attraction";
 
-const useMock = true
-//   import.meta.env.VITE_USE_MOCK === "true";
+const useMock = import.meta.env.VITE_USE_MOCK === "true";
 
 export const getAttractions =
-  async (): Promise<Attraction[]> => {
+    async (): Promise<Attraction[]> => {
 
-    if (useMock) {
-      return attractionsMock as Attraction[];
-    }
+        if (useMock) {
+            return attractionsMock as Attraction[];
+        }
 
-    const response =
-      await apiClient.get<Attraction[]>(
-        "/api/attractions"
-      );
+        const response =
+            await apiClient.get<Attraction[]>(
+                "/api/attractions/recommended"
+            );
 
-    return response.data;
-  };
+        return response.data;
+    };
